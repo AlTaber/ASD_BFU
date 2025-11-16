3b1dbe6475c7c79cfbb8fff23209cdf33c936a2a
AlTaber <Antonnikov07@bk.ru>
2025-11-16 17:13:42 +0200

    7 8 9 10

--- sortirovki.py
+++ sortirovki.py
@@ -1,16 +1,6 @@
 import time
 import random
 
-def measure_time(function):
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        result = function(*args, **kwargs)
-        end = time.time()
-        print(f"Время выполнения сортировки '{function.__name__}': {end - start:.9f} s")
-        return result
-    return wrapper
-
-@measure_time
 def comb_sort(arr):        # №4 Сортировка методом прочёсывания
     n = len(arr)
     step = n
@@ -26,7 +16,6 @@ def comb_sort(arr):        # №4 Сортировка методом прочё
           i += step
     return arr
 
-@measure_time
 def insertion_sort(arr):   # №5 Сортировка вставками
     for i in range(1, len(arr)):
         key = arr[i]
@@ -37,7 +26,6 @@ def insertion_sort(arr):   # №5 Сортировка вставками
         arr[j + 1] = key
     return arr
 
-@measure_time
 def selection_sort(arr):   # №6 Сортировка выбором
     for i in range(len(arr) - 1):
         min_index = i
@@ -46,27 +34,89 @@ def selection_sort(arr):   # №6 Сортировка выбором
                 arr[k], arr[min_index] = arr[min_index], arr[k]
     return arr
 
-@measure_time
 def shell_sort(arr):       # №7 Сортировка Шелла
-    pass
+    last_index = len(arr)
+    step = len(arr)//2
+    while step > 0:
+        for i in range(step, last_index, 1):
+            j = i
+            delta = j - step
+            while delta >= 0 and arr[delta] > arr[j]:
+                arr[delta], arr[j] = arr[j], arr[delta]
+                j = delta
+                delta = j - step
+        step //= 2
+    return arr
 
-@measure_time
 def radix_sort(arr):       # №8 Поразрядная сортировка
-    pass
+    max_digits = max([len(str(x)) for x in arr])
 
-@measure_time
+    base = 10
+
+    bins = [[] for _ in range(base)]
+
+    for i in range(0, max_digits):
+        for x in arr:
+            digit = (x // base ** i) % base
+            bins[digit].append(x)
+        arr = [x for queue in bins for x in queue]
+
+        bins = [[] for _ in range(base)]
+    return arr
+
 def heap_sort(arr):        # №9 Пирамидальная (heap sort)
-    pass
+    n = len(arr)
 
-@measure_time
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+    
+    return arr
+
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
 def merge_sort(arr):       # №10 Сортировка слиянием
-    pass
+    if len(arr) <= 1:
+        return arr
 
-@measure_time
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    merged = []
+    while left and right:
+        if left[0] < right[0]:
+            merged.append(left.pop(0))
+        else:
+            merged.append(right.pop(0))
+    merged.extend(left or right)
+    return merged
+
 def quick_sort(arr):       # №11 Быстрая сортировка
     pass
 
-@measure_time
 def external_sort(filename):  # №12 Внешняя многофазная сортировка
     pass
 
@@ -81,8 +131,12 @@ if __name__ == "__main__":
         print("⏺️  Применение сортировок для массива:")
         print(arr)
         print("---------------------------------------------")
-        for sort_function in [comb_sort, insertion_sort, selection_sort, ]: #shell_sort, radix_sort, heap_sort, merge_sort, quick_sort, sort]:
+        for sort_function in [comb_sort, insertion_sort, selection_sort, shell_sort, radix_sort, heap_sort, merge_sort, ]: #quick_sort]:
             a = arr.copy()
-            print('Успешно ✅' if sort_function(a) == A else 'Ошибка ❌')
+            start = time.time()
+            result = sort_function(a)
+            end = time.time()
+            time_result = end - start
+            print(sort_function.__name__ + (': Успешно ✅ ' if result == A else ': Ошибка ❌') + f" {time_result:.9f} s")
         print("---------------------------------------------")
         
